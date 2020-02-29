433cade03d2e5eab23307b3cc23d369dd3782168
Marie Hargitt <github@mail.usha.io>
2020-02-29 11:06:34 +0100

    Update js and update html

--- web/src/index.js
+++ web/src/index.js
@@ -1,41 +1,43 @@
 
+const url = 'http://localhost:4567/v1/indexations'
 const form = document.querySelector('#indexation-form');
 const list = document.querySelector('#results');
-
+const startOver = document.getElementById("start-over");
 const startDate = document.getElementById('start-date');
 const signedOn = document.getElementById('signed-on');
 const baseRent = document.getElementById('base-rent');
 
-
-
-const insertIndexationResults = (data) => {
-  data.forEach((result) => {
-    const new_rent = `<li>
-      New rent: ${result.new_rent}</li>
-      <li>Current index: ${result.current_index}</li>
-      <li>Base index: ${result.base_index}
-    </li>`;
-    list.insertAdjacentHTML('beforeend', new_rent);
-  });
+const updateForm = () => {
+  fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      list.innerHTML = "";
+      data.list.forEach((result) => {
+        const newRent = `<li>Your new rent is ${result.new_rent}, with a current index of ${result.current_index} and a base index of ${result.base_index}</li>`
+        list.insertAdjacentHTML("afterbegin", newRent);
+      });
+    });
 };
 
-
-const inputNewForm = (event) => {
-  const formData = { start_date: startDate.value, signed_on: signedOn.value, base_rent: baseRent.value };
-  fetch('http://localhost:4567/v1/indexations', {
+const postInputForm = (input, callback) => {
+  fetch(url, {
     method: "POST",
-    body: JSON.stringify(formData)
-  })
-    .then(response => response.json())
-    .then(insertIndexationResults);
+    body: JSON.stringify(input)
+  }).then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      callback();
+    });
 };
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  inputNewForm(event);
+  const data = { start_date: startDate.value, signed_on: signedOn.value, base_rent: baseRent.value };
+  postInputForm(data, updateForm);
 });
 
-
+startOver.addEventListener("click", updateForm);
+document.addEventListener("DOMContentLoaded", updateForm);
 
 
 
